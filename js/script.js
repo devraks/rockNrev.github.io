eebb8772c2ee2cb8f160f58b4ec44f574bab48d6
Rakesh N <128908046+RocknRev@users.noreply.github.com>
2023-05-19 09:03:23 +0530

    Files added

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var typed=new Typed(".typing",{
+    strings:["Web Developer", "Free Lancer", "System Associate"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwF24iC34GgscTaqWsjWHM-xgrpYdSN8smKo4oUjGQIweukvSoUTnitvwwYSwd5IbCG_A/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Submitted Successfully."
+        setTimeout(function(){
+            msg.innerHTML=""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
